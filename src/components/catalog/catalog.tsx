426077c45a0e732212818296cdc35e789ff07a8c
Danila Tikhonov <tikhonov3491@gmail.com>
2023-12-03 21:51:19 +0300

    add module6-task1

--- src/components/catalog/catalog.tsx
+++ src/components/catalog/catalog.tsx
@@ -1,27 +1,18 @@
-import { FilmInfoProps } from '../../types/film-types.ts';
-import Card from '../card/card';
+import FilmsList from '../films-list/films-list.tsx';
 import GenresList from '../genres-list/genres-list.tsx';
 import React from 'react';
 
-type CatalogProps = {
-    films: FilmInfoProps[];
-};
-
-export default function Catalog({ films }: CatalogProps): React.JSX.Element {
-    return (
-        <section className="catalog">
-            <h2 className="catalog__title visually-hidden">Catalog</h2>
-            <GenresList />
-            <div className="catalog__films-list">
-                {films.map((film) => (
-                    <Card film={film} key={film.name} />
-                ))}
-            </div>
-            <div className="catalog__more">
-                <button className="catalog__button" type="button">
-                    Show more
-                </button>
-            </div>
-        </section>
-    );
+export default function Catalog(): React.JSX.Element {
+  return (
+    <section className="catalog">
+      <h2 className="catalog__title visually-hidden">Catalog</h2>
+      <GenresList />
+      <FilmsList/>
+      <div className="catalog__more">
+        <button className="catalog__button" type="button">
+          Show more
+        </button>
+      </div>
+    </section>
+  );
 }
